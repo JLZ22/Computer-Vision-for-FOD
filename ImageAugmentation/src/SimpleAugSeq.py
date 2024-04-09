ae543d5f9ff891130692ddbf91bde3bb62005b86
John Zeng <johnzeng878@gmail.com>
2024-04-08 20:33:44 -0400

    write save function

--- ImageAugmentation/src/SimpleAugSeq.py
+++ ImageAugmentation/src/SimpleAugSeq.py
@@ -13,17 +13,17 @@ from pascal_voc_writer import Writer
 from imgaug.augmentables.bbs import BoundingBox, BoundingBoxesOnImage
 
 class SimpleAugSeq:
-    def __init__(self, path, save_path, seed, num_copies) -> None:
+    def __init__(self, path, save_path, seed, num_copies, names) -> None:
         self.path = path
         self.save_path = path
         self.seed = seed
         self.num_copies = num_copies
-        self.contents = os.listdir(path) 
+        self.names = names
         ia.seed(self.seed)
 
     # Return an array of copies of the image stored at 
     # path/img. The array has num_copies number of copies.
-    def make_copies_images(img: str, num_copies: int) -> np.array:
+    def make_copies_images(self, img: str, num_copies: int) -> np.array:
         return np.array(
             [il.imread(path + img) for _ in range(int)],
             dtype=np.uint8
@@ -31,10 +31,10 @@ class SimpleAugSeq:
 
     # Make num_copies number of the bbs object and return it 
     # in an array
-    def make_copies_bboxes(bbs: BoundingBoxesOnImage, num_copies: int) -> np.array:
+    def make_copies_bboxes(self, bbs: BoundingBoxesOnImage, num_copies: int) -> np.array:
         return [bbs for _ in range(num_copies)]
     
-    def create_sequential() -> iaa.Sequential:
+    def create_sequential(self) -> iaa.Sequential:
         return iaa.Sequential([  #randomly transforms the image
             iaa.Fliplr(0.5), # horizontal flips
             iaa.Crop(percent=(0, 0.1)), # random crops
@@ -71,7 +71,7 @@ class SimpleAugSeq:
     # given root and shape by automatically creating a 
     # new BoundingBox object for every object 
     # in the root
-    def create_bbs(root, shape: int) -> BoundingBoxesOnImage:
+    def create_bbs(self, root, shape: int) -> BoundingBoxesOnImage:
         bboxes = []
         for member in root.findall('object'):
             xmin = int(member[4][0].text)
@@ -81,18 +81,53 @@ class SimpleAugSeq:
             bboxes.append(BoundingBox(x1=xmin, y1=ymin, x2=xmax, y2=ymax))
         return BoundingBoxesOnImage(bboxes, shape)
     
+    # Save the images and corresponding xml files 
+    # 
+    # Naming convention: name + '_synth_' + i + '.file_extension'
+    # e.g. The third synthetic image of 18.jpg will be called
+    #      `18_synth_2.jpg`. Its xml file will have the same name.
+    def save_aug_pairs(self, 
+                       imgs: np.array, 
+                       bbss: np.array, 
+                       original_name: str, 
+                       height, 
+                       width, 
+                       class_name) -> None:
+        for i in range(imgs.shape[0]):
+            curr_path = self.save_path + original_name
+            img_path = curr_path + '.jpg'
+            xml_path = curr_path + '.xml'
+            il.imwrite(img_path, imgs[i])
+            writer = Writer(img_path, height, width)
+            for box in bbss[i]:
+                writer.addObject(class_name, box.x1, box.y1, box.x2, box.y2)
+
+            writer.save(xml_path)
+            
     # The primary function in charge of 
     # augmenting everything
     def augment(self):
-        for y in range(2): # Modifies 2 images starting at 18
-            name = str(y+18) # name of the image/xml pair we are considering
-
+        # augments every image in the list given to the constructor 
+        for name in self.names:
             # get the tree for the current xml file as well as its root
             tree = ET.parse(path + name + '.xml') 
             root = tree.getroot()
 
-            images = self.make_copies(name+'.jpg', self.num_copies) # make 64 copies of the current image and store it in a np.array
-            bbs = self.create_bbs(root, image[0].shape) # create the BoundingBoxesOnImage object for the current image
+            images = self.make_copies(name+'.jpg', self.num_copies) # make num_copies number of copies of the current image 
+            bbs = self.create_bbs(root, images[0].shape) # create the BoundingBoxesOnImage object for the current image
+            allbbs = self.make_copies_bboxes(bbs, self.num_copies) # make num_copies number of copies of the current image's corresponding xml file
+
+            seq = self.create_sequential() # create the sequential object in charge of the augmentation
+
+            images_aug, bbs_aug = seq(images=images, bounding_boxes=allbbs)
+                
+            #bbs_aug = bbs_aug.remove_out_of_image().clip_out_of_image() #This is an attempt to fix broken bounding boxes Current issue is list has no attribute 'remove_out_of_image()'
+            height = int(root.find("size")[0].text)
+            width = int(root.find("size")[1].text)
+            class_name = str(root.find('object')[0].text)
+            self.save_aug_pairs(images_aug, bbs_aug, name, height, width, class_name)
+
+
 
 # path on John laptop
 path = "smb://ecn-techwin.ecn.purdue.edu/Research/PLM/Restricted/Research/Project Folders/Active/ADT - Assembly Digital Thread/FOD/Images/pencils/Synthetic images/"
