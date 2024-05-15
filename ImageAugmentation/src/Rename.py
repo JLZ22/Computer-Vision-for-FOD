b0beef0a8b958d0cbd24f19083edf0336debaa95
John Zeng <johnzeng878@gmail.com>
2024-05-15 09:35:34 -0700

    remove unnecessary complex aug + add test folder + modify rename

--- ImageAugmentation/src/Rename.py
+++ ImageAugmentation/src/Rename.py
@@ -2,10 +2,14 @@ import os
 
 def rename(path, startIndex):
     for count, filename in enumerate(os.listdir(path)):
+        # get file extension of filename
+
+        if filename[-4:] != '.jpg':
+            continue
         print(path + filename)
         print(path + str(count) + '.jpg')
         os.rename(path + filename, path + str(count) + '.xml')
         
-path = '/Volumes/Research/PLM/Restricted/Research/Project Folders/Active/ADT - Assembly Digital Thread/FOD/Images/'
+path = '../test_data/test'
 path += 'Allen Wrench/'
 rename(path, 0)
