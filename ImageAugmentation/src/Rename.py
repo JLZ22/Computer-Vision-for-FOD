2c51286008dcbb20e38b7e328a805d579a95f316
John Zeng <johnzeng878@gmail.com>
2024-05-15 09:45:21 -0700

    rename script works

--- ImageAugmentation/src/Rename.py
+++ ImageAugmentation/src/Rename.py
@@ -1,15 +1,19 @@
 import os
 
 def rename(path, startIndex):
-    for count, filename in enumerate(os.listdir(path)):
-        # get file extension of filename
-
-        if filename[-4:] != '.jpg':
-            continue
-        print(path + filename)
-        print(path + str(count) + '.jpg')
-        os.rename(path + filename, path + str(count) + '.xml')
+    files = os.listdir(path)
+    files = [f for f in files if f.endswith('.jpg')]
+    files.sort()
+    for count, filename in enumerate(files, start=startIndex):
+        name = filename[:-4]
+        oldJPG = os.path.join(path, name + '.jpg')
+        oldXML = os.path.join(path, name + '.xml')
+        newJPG = os.path.join(path, str(count) + '.jpg')
+        newXML = os.path.join(path, str(count) + '.xml')
+        print(f'{oldJPG} -> {newJPG}')
+        print(f'{oldXML} -> {newXML}')
+        # os.rename(oldJPG, newJPG)
+        # os.rename(oldXML, newXML)
         
 path = '../test_data/test'
-path += 'Allen Wrench/'
 rename(path, 0)
