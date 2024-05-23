ac66ebb35651e3eb17f98eb4e2b9aef64e4aeeef
zeng261admix <jlzadmx@boilerad.purdue.edu>
2024-05-22 22:36:24 -0400

    update rename script to use pathlib

--- ImageAugmentation/src/Rename.py
+++ ImageAugmentation/src/Rename.py
@@ -1,20 +1,20 @@
-import os
+from pathlib import Path
 
 def rename(path, startIndex):
-    files = os.listdir(path)
-    files = [f for f in files if f.endswith('.jpg')]
+    files = [item for item in path.iterdir() if item.suffix.lower() in {'.jpg', '.jpeg'}]
     files.sort()
+    print(files)
     for count, filename in enumerate(files, start=startIndex):
-        name = filename[:-4]
-        oldJPG = os.path.join(path, name + '.jpg')
-        oldXML = os.path.join(path, name + '.xml')
-        newJPG = os.path.join(path, str(count) + '.jpg')
-        newXML = os.path.join(path, str(count) + '.xml')
+        name = filename.stem
+        oldJPG = Path(path, name + '.jpg')
+        oldXML = Path(path, name + '.xml')
+        newJPG = Path(path, str(count) + '.jpg')
+        newXML = Path(path, str(count) + '.xml')
         print(f'{oldJPG} -> {newJPG}')
         print(f'{oldXML} -> {newXML}')
-        # os.rename(oldJPG, newJPG)
-        # os.rename(oldXML, newXML)
+        oldJPG.rename(newJPG)
+        oldXML.rename(newXML)
         
-path = os.path.join('..', 'test_data', 'test')
-print(path)
-# rename(path, 0)
+path = Path('..', 'test_data', 'test')
+print(f'path: "{path}"')
+rename(path, 100)
