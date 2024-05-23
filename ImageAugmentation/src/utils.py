f515593533a3f5b45a82777181d3adbd253ad1bf
John Zeng <johnzeng878@gmail.com>
2024-05-23 17:15:27 -0400

    update utils working towards resizing boxes as well

--- ImageAugmentation/src/utils.py
+++ ImageAugmentation/src/utils.py
@@ -4,6 +4,10 @@ import imgaug.augmenters as iaa
 import cv2
 import numpy as np
 import os
+from pascal_voc_writer import Writer
+import xml.etree.ElementTree as ET
+import  xml.dom.minidom
+from imgaug.augmentables.bbs import BoundingBox, BoundingBoxesOnImage
 
 def print_red(text):
     print("\033[91m{}\033[0m".format(text))
@@ -46,7 +50,12 @@ def subtract_mean(image):
     image = image - mean
     return image
 
-def padAndResizeAllSquare(path, save_path, dim, batchsize = 16):
+def get_bboxes(path, jpg_files):
+    bbss = []
+    
+    return bbss
+
+def pad_and_resize_all_square(path, save_path, dim, batchsize = 16, bbss = False):
     # check read and write paths
     if not path.exists() or not path.is_dir():
         print_red(f"Directory: '{path}' does not exist or is not a directory.")
@@ -59,21 +68,29 @@ def padAndResizeAllSquare(path, save_path, dim, batchsize = 16):
         os.mkdir(str(save_path))
     
     # augmenters
-    res = iaa.Resize({'height' : dim, 'width' : dim})
-    pad = iaa.PadToSquare()
+    aug = iaa.Sequential([
+        iaa.PadToSquare(),
+        iaa.Resize({'height' : dim, 'width' : dim})
+    ])
 
     # read images, pad and resize
     try:
         image_files = list(path.glob('*.jpg')) + list(path.glob('*.jpeg'))
+        xml_files = list(path.glob('*.xml'))
         for i in range(0, len(image_files), batchsize):
             batch = image_files[i:i+batchsize]
             images = [cv2.imread(str(image_path)) for image_path in batch]
             resized = []
-            for image in images:
-                padded = pad.augment_image(image)
-                resized.append(res.augment_image(padded))
-            for i in range(len(resized)):
-                cv2.imwrite(str(save_path / str('resized_' + str(batch[i].name))) , resized[i])
+            res_bbss = []
+            if bbss:
+                boxes = get_bboxes(path, batch)
+                resized, res_bbss = aug(images=images, bounding_boxes=boxes)
+            else:
+                resized = aug(images=images)
+            for i, img in enumerate(resized):
+                cv2.imwrite(str(save_path / str('resized_' + str(batch[i].name))) , img)
+            if bbss:
+                ...
     except Exception as e:
         print(e)
         # if we created the save directory, delete it
