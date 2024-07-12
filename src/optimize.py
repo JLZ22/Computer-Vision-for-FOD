70ee82fc57f540f02b721eb7fb801c61bb2bd1dc
John L Zeng <zeng261@purdue.edu>
2024-07-12 13:48:54 -0400

    change optimize to read from yaml

--- src/optimize.py
+++ src/optimize.py
@@ -1,9 +1,16 @@
 from ultralytics import YOLO
 import utils
+import pathlib as Path
+import yaml
 
 if __name__ == '__main__':
+    data_path = Path("../test_data/dataset/dataset.yaml")
+    with open("../env.yaml") as f:
+        config = f.safe_load(f)
+    if config:
+        data_path = config['path']
     model = YOLO('../models/yolov8n.pt')
-    best = model.tune(data="../test_data/dataset/dataset.yaml", 
+    best = model.tune(data=data_path, 
                         epochs=1, 
                         iterations=10, 
                         imgsz=512,
