8b9cc883457ee3aa6c2c3a7ffa8cd168e88f621a
John Zeng <johnzeng878@gmail.com>
2024-07-25 18:45:57 -0700

    modify tune parameters and add documentation

--- src/optimize.py
+++ src/optimize.py
@@ -1,23 +1,24 @@
 from ultralytics import YOLO
-import utils
+import Utils
 from pathlib import Path
 import yaml
 
 if __name__ == '__main__':
     data_path = Path("../test_data/dataset/dataset.yaml")
-    with open("../opt_config.yaml") as f:
+    with open("../config.yaml") as f:
         config = yaml.safe_load(f)
     if config:
         data_path = config['data_path']
 
     model = YOLO('../models/yolov8n.pt')
-    best = model.tune(data=data_path, 
-                        epochs=config['epochs'], 
-                        iterations=config['iterations'], 
-                        imgsz=config['imgsz'],
-                        optimizer="AdamW", 
-                        plots=False, 
-                        save=False, 
-                        val=False,
-                        device=utils.get_device())
-    print(best)
+    best = model.tune(
+        data=data_path, 
+        epochs=config['epochs'], 
+        iterations=config['iterations'], 
+        imgsz=config['imgsz'],
+        optimizer="AdamW", 
+        plots=True,    # Generate and display training plots
+        save=True,     # Save model checkpoints and final weights
+        val=True,      # Evaluate the model on the validation set
+        device=Utils.get_device()  # Specify device for training based on availability
+    )
