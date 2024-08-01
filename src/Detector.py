1b28087cb72015fbe1def0c3cbfb7eb4f16ccff1
John Zeng <johnzeng878@gmail.com>
2024-08-01 16:57:34 -0400

    update how results are stored and interpreted

--- src/Detector.py
+++ src/Detector.py
@@ -2,39 +2,25 @@ from ultralytics import YOLO
 import cv2
 import math
 
-allNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush"
-              ]
-
-FODNames = ['allen wrench', 'pencil', 'screwdriver', 'tool bit', 'wrench']
-
 class Detector:
     '''
     Wrapper class for the YOLO model to detect objects in
     images, videos, or camera streams.
     '''
 
-    def __init__(self, 
-                 model=         YOLO("../models/yolov8n.pt"),
-                 class_names=   allNames):
+    def __init__(self, model= YOLO("../models/yolov8n.pt"), class_names=None):
         
         self.model=model
-        self.classNames=class_names
+        if class_names is None:
+            self.class_names = model.names
 
     def interpret_frame_result(self, results, frame, show):
         '''
         Show function to display the bounding boxes and class names
         along with other FOD details and custom functionalities. 
         '''
-        for r in results:
+        r = next(results, None)
+        if r:
             boxes = r.boxes
             for box in boxes:
                 # bounding box
@@ -58,12 +44,12 @@ class Detector:
                 org = [x1 + 5, y1+25]
                 font = cv2.FONT_HERSHEY_SIMPLEX
                 fontScale = 1
-                color = (255, 0, 0)
+                color = (0, 255, 255)
                 thickness = 2
 
-                cv2.putText(frame, self.classNames[cls] + ' ' + str(confidence), org, font, fontScale, color, thickness)
-            if show:
-                cv2.imshow("Image", frame)
+                cv2.putText(frame, self.class_names[cls] + ' ' + str(confidence), org, font, fontScale, color, thickness)
+        if show:
+            cv2.imshow("Image", frame)
 
     def detect_camera(self, 
                      confidence=    0.7,
@@ -71,7 +57,8 @@ class Detector:
                      show=          True
                      ):
         '''
-        Detect objects in a camera stream.
+        Detect objects in a camera stream and highlights objects 
+        that are not supposed to be in a certain space.
         '''
         cap = cv2.VideoCapture(camera)
         while True:
@@ -96,10 +83,11 @@ class Detector:
         '''
         Detect objects in images or videos.
         '''
-        for vid in media_paths:
-            self.model.predict(vid, 
-                               show=        show, 
+        for media in media_paths:
+            results = self.model.predict(media, 
+                               show=        False, 
                                conf=        confidence,)
+            # TODO: use interpret_frame_result to show the results
     
     def detect(self,  
                input_type,
@@ -113,8 +101,8 @@ class Detector:
         results if a save path is provided.
         '''
         if input_type == 'media':
-            self.detect_media(confidence, media_paths)
+            self.detect_media(confidence, media_paths, show)
         elif input_type == 'camera':
-            self.detect_camera(confidence, camera)
+            self.detect_camera(confidence, camera, show)
         else:
             raise ValueError("Invalid input type. Please choose either 'media' or 'camera'.")
--- src/main.py
+++ src/main.py
@@ -2,6 +2,21 @@ from ultralytics import YOLO
 from Detector import Detector
 import yaml
 
+
+allNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+FODNames = ['allen wrench', 'pencil', 'screwdriver', 'tool bit', 'wrench']
+
 '''
 The different yolov8 models have varying levels of accuracy and speed.
 This is the order of the models from fastest to slowest (and least to most accurate):
@@ -19,5 +34,5 @@ if __name__ == "__main__":
         config = yaml.safe_load(f)
     detect = config['detect']
     model = YOLO(detect['model_path'])
-    detector = Detector(model, detect['media_paths'])
+    detector = Detector(model=model)
     detector.detect('camera')
