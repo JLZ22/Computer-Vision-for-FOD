bfa6fbe904bb3e4f0c570642a03e8f071a4aa254
John Zeng <johnzeng878@gmail.com>
2024-08-03 19:42:57 -0400

    update media detection to use custom interpret function

--- src/Detector.py
+++ src/Detector.py
@@ -14,7 +14,7 @@ class Detector:
         if class_names is None:
             self.class_names = model.names
 
-    def interpret_frame_result(self, results, frame, show):
+    def interpret_frame_result(self, results, frame, show, input_type, win_name):
         '''
         Show function to display the bounding boxes and class names
         along with other FOD details and custom functionalities. 
@@ -48,8 +48,14 @@ class Detector:
                 thickness = 2
 
                 cv2.putText(frame, self.class_names[cls] + ' ' + str(confidence), org, font, fontScale, color, thickness)
-        if show:
-            cv2.imshow("Image", frame)
+        if input_type == 'Image' and show:
+            while True:
+                cv2.imshow(win_name, frame)
+                if cv2.waitKey(1) == ord('q'):
+                    break
+            cv2.destroyWindow(win_name)
+        elif show:
+            cv2.imshow(win_name, frame)
 
     def detect_camera(self, 
                      confidence=    0.7,
@@ -61,6 +67,7 @@ class Detector:
         that are not supposed to be in a certain space.
         '''
         cap = cv2.VideoCapture(camera)
+        win_name = f'Camera {camera}'
         while True:
             ret, frame = cap.read()
             if not ret:
@@ -70,25 +77,44 @@ class Detector:
                                          conf=      confidence, 
                                          stream=    True)
             
-            self.interpret_frame_result(results, frame, show)
+            self.interpret_frame_result(results, frame, show, 'Camera', win_name)
             if cv2.waitKey(1) == ord('q'):
                 break
         cap.release()
-        cv2.destroyAllWindows()
+        cv2.destroyWindow(win_name)
 
     def detect_media(self, 
                     confidence,
                     media_paths,
                     show):  
         '''
-        Detect objects in images or videos.
+        Detect objects in images or videos. Shows results using 
+        custom function interpret_frame_result.
         '''
-        for media in media_paths:
-            results = self.model.predict(media, 
-                               show=        False, 
-                               conf=        confidence,)
-            # TODO: use interpret_frame_result to show the results
-    
+        for media_path in media_paths:
+            # read media_path and get the frame
+            frame = cv2.imread(media_path)
+            if frame is not None:
+                results = self.model.predict(frame, 
+                                                show=      False, 
+                                                conf=      confidence, 
+                                                stream=    True)
+                self.interpret_frame_result(results, frame, show, 'Image', media_path)
+            else:
+                cap = cv2.VideoCapture(media_path)
+                while True:
+                    ret, frame = cap.read()
+                    if not ret:
+                        break
+                    results = self.model.predict(frame, 
+                                                    show=      False, 
+                                                    conf=      confidence, 
+                                                    stream=    True)
+                    self.interpret_frame_result(results, frame, show, 'Video', media_path)
+                    if cv2.waitKey(1) == ord('q'):
+                        break
+                cv2.destroyWindow(media_path)
+            
     def detect(self,  
                input_type,
                confidence=  0.7,
@@ -98,9 +124,13 @@ class Detector:
                show=        True):
         '''
         Detect objects in images, videos, or camera streams. Saves the 
-        results if a save path is provided.
+        results if a save path is provided. If the input type is 'media',
+        the media paths should be provided. If the input type is 'camera',
+        the camera number should be provided.
+        TODO: Add the ability to save the results to a file.
         '''
         if input_type == 'media':
+            media_paths = [str(media_path) for media_path in media_paths]
             self.detect_media(confidence, media_paths, show)
         elif input_type == 'camera':
             self.detect_camera(confidence, camera, show)
